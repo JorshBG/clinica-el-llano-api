58e1ce6d1239a243b8894fb540a73cdc1e18fb89
JorshBG <jordiyair29@gmail.com>
2023-03-27 04:26:26 -0600

    Middleware added

--- models/Connection.php
+++ models/Connection.php
@@ -13,6 +13,8 @@ private static string $dsn = "mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";po
     {
         $conn = new PDO( Connection::$dsn, DBUSER, DBPASS);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        $conn->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $conn;
     }
 }
