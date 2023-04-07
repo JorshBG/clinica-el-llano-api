be41947c26c4d686c042544980044f6710223432
JorshBG <jordiyair29@gmail.com>
2023-04-07 13:48:37 -0600

    New dropdowns menus

--- views/pages/partials/SideMenusAdmin.php
+++ views/pages/partials/SideMenusAdmin.php
@@ -15,12 +15,12 @@
 </li>
 
 
-<!--  CREA UN DROPDOWN  -->
+<!--  Dropdown CATEGORIAS  -->
 <li class="nav-item">
             <span
                 class="nav-link collapsed d-flex justify-content-between align-items-center"
                 data-bs-toggle="collapse"
-                data-bs-target="#submenu-app">
+                data-bs-target="#categories">
                 <span>
                     <span class="sidebar-icon"><svg
                             class="icon icon-xs me-2"
@@ -47,7 +47,7 @@
     <div
         class="multi-level collapse"
         role="list"
-        id="submenu-app"
+        id="categories"
         aria-expanded="false">
         <ul class="flex-column nav">
 
@@ -56,10 +56,179 @@
             <li class="nav-item">
                 <a
                     class="nav-link"
-                    href="/dashboard/views/catalogo/presentacion"
-                ><span class="sidebar-text">Presentación</span></a
+                    href="/dashboard/views/catalogo/productos"
+                ><span class="sidebar-text">Productos</span></a
                 >
             </li>
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/proveedores"
+                ><span class="sidebar-text">Proveedores</span></a
+                >
+            </li>
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/unidad-de-medida"
+                ><span class="sidebar-text">Unidad de medida</span></a
+                >
+            </li>
+
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/categorias"
+                ><span class="sidebar-text">Categorías</span></a
+                >
+            </li>
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/almacenes"
+                ><span class="sidebar-text">Almacenes</span></a
+                >
+            </li>
+
+
+        </ul>
+    </div>
+</li>
+<!--  Dropdown COMPRAS  -->
+<li class="nav-item">
+            <span
+                class="nav-link collapsed d-flex justify-content-between align-items-center"
+                data-bs-toggle="collapse"
+                data-bs-target="#buys">
+                <span>
+                    <span class="sidebar-icon"><svg
+                            class="icon icon-xs me-2"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        fill-rule="evenodd"
+                        d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                        clip-rule="evenodd"></path>
+                  </svg> </span>
+
+                    <!--     TEXTO DEL DROPDOWN ANTES DE ABRIRSE       -->
+                    <span class="sidebar-text">Compras</span> </span><span class="link-arrow"><svg
+                        class="icon icon-sm"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                  <path
+                      fill-rule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd"></path></svg></span></span>
+    <div
+        class="multi-level collapse"
+        role="list"
+        id="buys"
+        aria-expanded="false">
+        <ul class="flex-column nav">
+
+            <!--     ITEMS DEL DROPDOWN       -->
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/ordenes-de-pago"
+                ><span class="sidebar-text">Ordenes de pago</span></a
+                >
+            </li>
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/pagos"
+                ><span class="sidebar-text">Pagos</span></a
+                >
+            </li>
+
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/categorias"
+                ><span class="sidebar-text">Entradas</span></a
+                >
+            </li>
+
+
+
+        </ul>
+    </div>
+</li>
+<!--  Dropdown Administracion de almacenes  -->
+<li class="nav-item">
+            <span
+                class="nav-link collapsed d-flex justify-content-between align-items-center"
+                data-bs-toggle="collapse"
+                data-bs-target="#admin">
+                <span>
+                    <span class="sidebar-icon"><svg
+                            class="icon icon-xs me-2"
+                            fill="currentColor"
+                            viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        fill-rule="evenodd"
+                        d="M5 4a3 3 0 00-3 3v6a3 3 0 003 3h10a3 3 0 003-3V7a3 3 0 00-3-3H5zm-1 9v-1h5v2H5a1 1 0 01-1-1zm7 1h4a1 1 0 001-1v-1h-5v2zm0-4h5V8h-5v2zM9 8H4v2h5V8z"
+                        clip-rule="evenodd"></path>
+                  </svg> </span>
+
+                    <!--     TEXTO DEL DROPDOWN ANTES DE ABRIRSE       -->
+                    <span class="sidebar-text text-center">Administración</span> </span><span class="link-arrow"><svg
+                        class="icon icon-sm"
+                        fill="currentColor"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                  <path
+                      fill-rule="evenodd"
+                      d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                      clip-rule="evenodd"></path></svg></span></span>
+    <div
+        class="multi-level collapse"
+        role="list"
+        id="admin"
+        aria-expanded="false">
+        <ul class="flex-column nav">
+
+            <!--     ITEMS DEL DROPDOWN       -->
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/ordenes-de-pago"
+                ><span class="sidebar-text">Entradas</span></a
+                >
+            </li>
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/pagos"
+                ><span class="sidebar-text">Traspaso</span></a
+                >
+            </li>
+
+
+            <li class="nav-item">
+                <a
+                    class="nav-link"
+                    href="/dashboard/views/catalogo/categorias"
+                ><span class="sidebar-text">Salidas</span></a
+                >
+            </li>
+
 
 
         </ul>
