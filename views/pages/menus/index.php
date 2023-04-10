b1138dc946736073a89579b0fe2d8ed5afcf94d3
JorshBG <jordiyair29@gmail.com>
2023-04-10 10:02:58 -0600

    New index view for dashboard

---
+++ views/pages/menus/index.php
@@ -0,0 +1,7 @@
+<div class="container-fluid h-fluid" >
+    <div class="d-grid h-full center-grid">
+        <h1 class="display-1">
+            <span class="text-dark">¡Bienvenido!</span>
+        </h1>
+    </div>
+</div>
