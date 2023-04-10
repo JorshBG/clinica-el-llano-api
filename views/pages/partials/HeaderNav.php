39adb29d8f0769a6de3d6b6791643664ab09e80c
JorshBG <jordiyair29@gmail.com>
2023-04-10 10:02:25 -0600

    Fixing...

--- views/pages/partials/HeaderNav.php
+++ views/pages/partials/HeaderNav.php
@@ -18,5 +18,30 @@
         <h2 class="h4"><?php echo $title?></h2>
         <p class="mb-0"><?php echo $subtitle??'' ?></p>
     </div>
-    <?php echo $button_component??'' ?>
+    <div class="btn-toolbar mb-2 mb-md-0">
+          <button 
+            type="button"
+            class="btn btn-sm btn-gray-800 d-inline-flex align-items-center"
+            data-bs-toggle="modal"
+            data-bs-target="#dashboard_modal-<?php echo $id_modal ?>"
+            >
+            <svg
+              class="icon icon-xs me-2"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 6v6m0 0v6m0-6h6m-6 0H6"
+              ></path>
+            </svg>
+            Nuev@ <?php echo $text_button??'' ?></a
+          >
+    </div>
+    
 </div>
+
