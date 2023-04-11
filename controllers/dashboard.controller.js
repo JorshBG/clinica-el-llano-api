39017f9f031c6c3d08d6e4faf17b2d51288c66ac
JorshBG <jordiyair29@gmail.com>
2023-04-10 18:23:09 -0600

    fix dashboard

--- controllers/dashboard.controller.js
+++ controllers/dashboard.controller.js
@@ -1,77 +1,76 @@
-import api from "/config/api.js"
-import RowTable from "/views/pages/components/RowTable.js"
-import { verifyDataTable, createDataTable } from "/views/pages/components/DataTablesModule.js"
+import api from "/config/api.js";
+import RowTable from "/views/pages/components/RowTable.js";
+import {
+  verifyDataTable,
+  createDataTable,
+} from "/views/pages/components/DataTablesModule.js";
 
+document.addEventListener("DOMContentLoaded", async () => {
+  let elementsOnTable = {};
+  let defaultAmountElements = 5;
+  let amountElements = defaultAmountElements;
 
-document.addEventListener('DOMContentLoaded', async () => {
+  const btnLogout = document.querySelector("#dashboard_btnLogout");
 
-    let elementsOnTable = {}
-    let defaultAmountElements = 5
-    let amountElements = defaultAmountElements
+  btnLogout.onclick = () => sign_out();
 
-    const btnLogout = document.querySelector('#dashboard_btnLogout');
+  const usernameLabel = document.getElementsByClassName("username-label");
 
-    btnLogout.onclick = () => sign_out();
+  for (const element of usernameLabel) {
+    element.innerText = sessionStorage.getItem("name");
+  }
 
-    const usernameLabel = document.getElementsByClassName('username-label')
+  const menus = document.getElementsByClassName("nav-only-item");
 
-    for (const element of usernameLabel) {
-        element.innerText = sessionStorage.getItem('name')
-    }
+  const bodyContent = document.getElementById("mainContent");
 
-    const menus = document.getElementsByClassName('nav-only-item')
+  for (const menu of menus) {
+    menu.addEventListener("click", async function (evt) {
+      evt.preventDefault();
+      bodyContent.innerHTML = await renderView(menu.pathname);
 
-    const bodyContent = document.getElementById('mainContent');
+      const bodyTable = document.querySelector("#dashboard_table-body");
 
-    for (const menu of menus) {
-        menu.addEventListener('click', async function (evt) {
-            evt.preventDefault();
-            bodyContent.innerHTML = await renderView(menu.pathname)
+      if (menu.pathname !== "/dashboard/view/index") {
+        let data = (await loadContentTable(menu.pathname)) ?? false;
+        verifyDataTable();
+        if (data) {
+          elementsOnTable = data.result;
+          let index = 1;
+          for (const product of elementsOnTable) {
+            let rowProduct = RowTable(product, index);
+            bodyTable.appendChild(rowProduct);
+            index++;
+          }
+        }
+        createDataTable();
+      }
+    });
+  }
+});
 
-            const bodyTable = document.querySelector('#dashboard_table-body');
-            
-            if(menu.pathname !== '/dashboard/view/index'){
-                let data = await loadContentTable(menu.pathname)
-                elementsOnTable = data.result
-                let index = 1
-                verifyDataTable()
-                for (const product of elementsOnTable) {
-                    let rowProduct = RowTable(product, index)
-                    bodyTable.appendChild(rowProduct)
-                    index++
-                }
-                createDataTable()
-            }
+function sign_out() {
+  sessionStorage.clear();
 
-        })
-    }
-
-})
-
-function sign_out(){
-
-    sessionStorage.clear();
-
-    window.location.href = '/sign-out'
-
+  window.location.href = "/sign-out";
 }
 
 async function renderView(ref) {
+  let res = await api(ref);
 
-    let res = await api(ref)
-
-    return await res.text()
-
+  return await res.text();
 }
 
-async function loadContentTable(ref){
-
-    let res = await api('/api' + ref, 'GET', null, {
-        'x-api-key': sessionStorage.getItem('token')
-    })
-
-    let dataJson = await res.json()
-
-    return dataJson
-
+async function loadContentTable(ref) {
+  let res = await api("/api" + ref, "GET", null, {
+    "x-api-key": sessionStorage.getItem("token"),
+  });
+  if (res.ok) {
+    try {
+      let dataJson = await res.json();
+      return dataJson;
+    } catch (error) {
+      return false;
+    }
+  }
 }
